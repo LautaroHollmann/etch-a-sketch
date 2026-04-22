89c64e01d09d6df4da2998cfb408b74876b9f646
Lautaro Hollmann <l.hollmann.lh@gmail.com>
2026-04-22 18:12:06 +0200

    adds a container div and 16 divs within it with a for loop

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
-// create 16 square divs
-// create.Element...
-// append them to the container div
-// container.appendChild("...")
-// add css to the container and the other 16 divs to make them appear in a 16x16 grid with flexbox within the container div
+const container = document.querySelector(".container")
+
+// Create 16 divs with a loop and append them to the container div:
+let divs = ["div1", "div2", "div3", "div4", "div5", "div6", "div7", "div8", "div9", "div10", "div11", "div12", "div13", "div14", "div15", "div16"];
+for(let i = 0; i < divs.length; i++) {
+    let divs16 = document.createElement("div");
+    divs16.className = "divs16";
+    divs16.innerText = `${divs[i]}`;
+    container.appendChild(divs16);
+}
+
+// make these 16 divs flex and square in a 16x16 grid within the container div!
