02031f57bf2dbade1df68b5324956531d9def021
Lautaro Hollmann <l.hollmann.lh@gmail.com>
2026-04-22 17:09:28 +0200

    adds html skeleton + container div  and links js and css files

---
+++ javascript.js
@@ -0,0 +1,5 @@
+// create 16 square divs
+// create.Element...
+// append them to the container div
+// container.appendChild("...")
+// add css to the container and the other 16 divs to make them appear in a 16x16 grid with flexbox within the container div
