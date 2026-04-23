459bd71e4378a428b8b04334d7d2d8c657a86c64
Lautaro Hollmann <l.hollmann.lh@gmail.com>
2026-04-23 19:50:24 +0200

    adds a loop for creation of 256 divs and css for griz and div size

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 const container = document.querySelector(".container")
 
-// Create 16 divs with a loop and append them to the container div:
-let divs = ["div1", "div2", "div3", "div4", "div5", "div6", "div7", "div8", "div9", "div10", "div11", "div12", "div13", "div14", "div15", "div16"];
-for(let i = 0; i < divs.length; i++) {
-    let divs16 = document.createElement("div");
-    divs16.className = "divs16";
-    divs16.innerText = `${divs[i]}`;
-    container.appendChild(divs16);
+// Create 16x16 (256) divs with a loop and append them to the container div:
+let divs = [""];
+
+for (let i=0; i<256; i++) {
+    divs.push(i);
+    let gridDivs = document.createElement("div");
+    gridDivs.className = "gridDivs";
+    container.appendChild(gridDivs);
 }
 
-// make these 16 divs flex and square in a 16x16 grid within the container div!
+
+// make these 256 divs flex and square in a 16x16 grid within the container div!
